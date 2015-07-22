00550b22404dc82848e870405f7e4e282e7daa9a
Anthony Jackman <energeticpixels@gmail.com>
2015-07-22 00:14:03 -0500

    adding get and post into one chained statement

--- routes/movies.js
+++ routes/movies.js
@@ -2,22 +2,21 @@ var Movie = require('../models/movie');
 var express = require('express');
 var router = express.Router();
 
-router.route('/movies').get(function(req, res) {
-	Movie.find(function(err, movies) {
-		if (err) {
-			return res.send(err);
-		}
-		res.json(movies);
+router.route('/movies')
+	.get(function(req, res) {
+		Movie.find(function(err, movies) {
+			if (err) {
+				return res.send(err);
+			}
+			res.json(movies);
+		});
+	})
+	.post(function(req, res) {
+		var movie = new Movie(req.body);
+		movie.save(function(err) {
+			if (err) {
+				return res.send(err);
+			}
+			res.send({message: 'Movie Added'});
+		});
 	});
-});
-
-router.route('/movies').post(function(req, res) {
-	var movie = new Movie(req.body);
-	
-	movie.save(function(err) {
-		if (err) {
-			return res.send(err);
-		}
-		res.send({message: 'Movie Added'});
-	});
-});
