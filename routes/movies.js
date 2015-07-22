8b9ddbcb8069902c567127e3b17efd9e068d947a
Anthony Jackman <energeticpixels@gmail.com>
2015-07-22 00:09:50 -0500

    adding routes to movies get and post

---
+++ routes/movies.js
@@ -0,0 +1,23 @@
+var Movie = require('../models/movie');
+var express = require('express');
+var router = express.Router();
+
+router.route('/movies').get(function(req, res) {
+	Movie.find(function(err, movies) {
+		if (err) {
+			return res.send(err);
+		}
+		res.json(movies);
+	});
+});
+
+router.route('/movies').post(function(req, res) {
+	var movie = new Movie(req.body);
+	
+	movie.save(function(err) {
+		if (err) {
+			return res.send(err);
+		}
+		res.send({message: 'Movie Added'});
+	});
+});
